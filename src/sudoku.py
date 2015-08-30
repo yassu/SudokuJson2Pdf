81bce609345bce146451c5035caa1e27abad64bb
yuki <mathyassu@gmail.com>
2015-08-31 08:28:53 +0900

    Sudoku.write_pdf

--- src/sudoku.py
+++ src/sudoku.py
@@ -1,8 +1,14 @@
 from json import loads as _json_loads
 from json import load  as _json_load
+from reportlab.pdfgen import canvas
 
 SUDOKU_SIZE = (9, 9)
 CELL_SIZE = (3, 3)
+FONT_SIZE = 30
+SUDOKU_PDF_OFFSET = (50, 50)
+SUDOKU_XPAD, SUDOKU_YPAD = (5, 5)
+DIFFERENCE_FOR_NUMBERS = ((FONT_SIZE + SUDOKU_XPAD)//2,
+                          (FONT_SIZE + SUDOKU_YPAD)//2)
 
 class Sudoku(object):
 
@@ -33,7 +39,9 @@ class Sudoku(object):
         return self.mat == other.mat
 
 class SudokuPageInfo(object):
-    def __init__(self, sudoku, header, footer, show_page_number=False):
+
+    def __init__(self, sudoku, header, footer,
+            show_page_number=False, page_num=None):
         self._sudoku = sudoku
         self._header = header
         self._footer = footer
@@ -54,3 +62,40 @@ class SudokuPageInfo(object):
     @property
     def show_page_number(self):
         return self._show_page_number
+
+    def write_pdf(self, canvas, offset=SUDOKU_PDF_OFFSET):
+        xlist = [offset[0] + j*(2*XPAD + FONT_SIZE) for j in range(0, 9 + 1)]
+        ylist = [offset[1] + j*(2*YPAD + FONT_SIZE) for j in range(0, 9 + 1)]
+        canvas.grid(xlist, ylist)
+        for x in range(0, 9):
+            for y in range(0, 9):
+                canvas.drawString(
+                    offset[0] + (FONT_SIZE + XPAD)//2
+                        + x*(2*SUDOKU_XPAD + FONT_SIZE) - 4,
+                    offset[1] + FONT_SIZE
+                        + y*(2*SUDOKU_YPAD + FONT_SIZE),
+                    str(self.sudoku.mat[x][y])
+                    )
+
+if __name__ == '__main__':
+    sudoku = Sudoku([
+            [1, 8, 7, 4, 9, 3, 6, 5, 2],
+            [6, 4, 3, 1, 2, 5, 8, 9, 7],
+            [2, 5, 9, 7, 8, 6, 3, 1, 4],
+
+            [7, 1, 4, 2, 3, 8, 5, 6, 9],
+            [9, 6, 8, 5, 7, 1, 4, 2, 3],
+            [5, 3, 2, 6, 4, 9, 1, 7, 8],
+
+            [8, 7, 6, 9, 1, 4, 2, 3, 5],
+            [4, 9, 1, 3, 5, 2, 7, 8, 6],
+            [3, 2, 5, 8, 6, 7, 9, 4, 1]
+        ])
+    sudoku_info = SudokuPageInfo(sudoku, 'header', 'footer',
+        show_page_number=True, page_num=3)
+
+    c = canvas.Canvas("sample.pdf", bottomup=False)
+    XPAD, YPAD = (5, 5)
+    c.setFont("Helvetica", FONT_SIZE)
+    sudoku_info.write_pdf(c, offset=(100, 100))
+    c.save()
